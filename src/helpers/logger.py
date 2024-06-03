d363423c0b52661941e1a9092e89d9fdbeff81a2
Jeremiaszmacura <jeremiaszmacura99@gmail.com>
2024-06-03 19:24:07 +0200

    work

--- src/helpers/logger.py
+++ src/helpers/logger.py
@@ -2,9 +2,31 @@
 import os
 import logging
 import datetime
+import pathlib
 
 
-logger = logging.getLogger(__name__)
-logs_path = f"logs/{datetime.datetime.now()}-stock-app.log"
-os.makedirs(os.path.dirname(logs_path), exist_ok=True)
-logging.basicConfig(filename=logs_path, level=logging.INFO)
+def create_logger(logs_dir: str) -> tuple:
+    os.makedirs(logs_dir, exist_ok=True)
+    time_stamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    logs_filename = f"{time_stamp}-stock-app.log"
+    logs_file_patch = os.path.join(logs_dir, logs_filename)
+
+    logging.basicConfig(filename=logs_file_patch, level=logging.INFO)
+
+    logger = logging.getLogger(__name__)
+
+    return logger, logs_file_patch
+
+
+def get_logs_dir() -> str:
+    current_dir_name = pathlib.Path(__file__).parent
+    module_src_path, _ = os.path.split(current_dir_name)
+    module_main_path, _ = os.path.split(module_src_path)
+    logs_dir_name = "logs"
+    logs_dir = os.path.join(module_main_path, logs_dir_name)
+    return logs_dir
+
+
+logs_dir = get_logs_dir()
+logger, logs_file_patch = create_logger(logs_dir)
+logging.info(f"Logger configured. Logs path: {logs_file_patch}")
---
+++ tests/entrypoints/test_cli_entrypoint.py
@@ -0,0 +1,18 @@
+import unittest
+from unittest.mock import patch, MagicMock
+from typer.testing import CliRunner
+
+from src.entrypoints.cli_entrypoint import app
+
+
+runner = CliRunner()
+
+
+class TestSearchForCompany(unittest.TestCase):
+
+    @patch("keyring.get_password")
+    def test_search_for_company(self, get_password):
+        get_password.return_value = MagicMock()
+        result = runner.invoke(app, ["search-for-company", "--phrase", "INTC"])
+        assert result.exit_code == 0
+        get_password.assert_called_once()
